a1d4de4ddef3d416444c1a522c427973423b5668
Naim Salameh <naim.salameh@consensys.net>
2023-01-08 17:33:26 +0200

    lesson3

--- Solutions/Lesson 2/main.py
+++ Solutions/Lesson 2/main.py
@@ -1,6 +1,13 @@
 # Fix the code below 👇
 
+# Missing double quotes before the word Day
 print("Day 1 - String Manipulation")
-print("String Concatenation is done with the " + '"+"' + " sign.")
+
+# Outer double quotes changed to single
+print('String Concatenation is done with the ' + "+" + 'sign.')
+
+# Extra indentation remove
 print('e.g. print("Hello " + "world")')
+
+# Extra ( in print function removed
 print("New lines can be created with a backslash and n.")
