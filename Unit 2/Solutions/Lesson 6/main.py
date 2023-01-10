cf470cae5f865ba69aba73453c4f10c818b94293
Naim Salameh <no3am@users.noreply.github.com>
2023-01-10 13:43:00 +0200

    lesson6 (#6)
    Co-authored-by: Naim Salameh <naim.salameh@consensys.net>

---
+++ Unit 2/Solutions/Lesson 6/main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+
+digit_one = two_digit_number[0]
+digit_two = two_digit_number[1]
+
+total = int(digit_one) + int(digit_two)
+
+print(total)
